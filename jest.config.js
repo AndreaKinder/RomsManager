824c2c2fbc82adc3e2178eb2f6b345bbd72a2b9f
Andrea <andvillart@gmail.com>
2025-11-30 18:04:47 +0100

    fix: ubication test

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,19 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    '^nes\\.css/css/nes\\.min\\.css$': 'identity-obj-proxy'
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "^nes\\.css/css/nes\\.min\\.css$": "identity-obj-proxy",
   },
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  testMatch: [
-    '**/__tests__/**/*.(test|spec).(js|jsx)',
-    '**/*.(test|spec).(js|jsx)'
-  ],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testMatch: ["<rootDir>/tests/**/*.(test|spec).(js|jsx)"],
   collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!src/**/*.test.{js,jsx}',
-    '!src/index.js',
-    '!src/renderer.js'
-  ]
+    "src/**/*.{js,jsx}",
+    "!src/**/*.test.{js,jsx}",
+    "!src/index.js",
+    "!src/renderer.js",
+  ],
 };
