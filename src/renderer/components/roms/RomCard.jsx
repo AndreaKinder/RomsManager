fa7ebeecf96dc2e5988f2bb41516604bdc6fe91c
Andrea <andvillart@gmail.com>
2025-12-09 05:42:55 +0100

    feat: add download rom button

--- src/renderer/components/roms/RomCard.jsx
+++ src/renderer/components/roms/RomCard.jsx
@@ -122,6 +122,28 @@ function RomCard({ rom, onRomUpdated }) {
               <path d="m15 5 4 4" />
             </svg>
           </button>
+          <button
+            className="rom-icon-btn btn-download"
+            onClick={handleDownloadClick}
+            disabled={isDeleting}
+            title="Descargar ROM"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+              <polyline points="7 10 12 15 17 10" />
+              <line x1="12" y1="15" x2="12" y2="3" />
+            </svg>
+          </button>
         </div>
       </div>
 
