2e8660cef66d50afa125b3b602126bb2634e3743
Andrea <andvillart@gmail.com>
2025-12-09 02:19:33 +0100

    feat: Add editRomName to editService

--- src/back/services/editService.js
+++ src/back/services/editService.js
@@ -3,3 +3,7 @@ import { updateRomInJson } from "./utils/getJsonUtils.js";
 export function editRomTitle(romName, newRomTitle) {
   return updateRomInJson(romName, "title", newRomTitle);
 }
+
+export function editRomName(romName, newRomName) {
+  return updateRomInJson(romName, "romName", newRomName);
+}
