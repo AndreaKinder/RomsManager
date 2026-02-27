30a4e9cebb4fde81fd2cdbda187cd3933910c82c
Andrea <andvillart@gmail.com>
2026-02-27 03:01:31 +0100

    refactor(forge.config.js): convert require to ES6 imports

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,7 @@
-const { FusesPlugin } = require("@electron-forge/plugin-fuses");
-const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
 
-module.exports = {
+export default {
   packagerConfig: {
     asar: true,
     icon: "./assets/icon",
@@ -11,7 +11,6 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    // Windows installer (Squirrel.Windows)
     {
       name: "@electron-forge/maker-squirrel",
       platforms: ["win32"],
@@ -26,7 +25,6 @@ module.exports = {
         noMsi: true,
       },
     },
-    // Windows portable (ZIP)
     {
       name: "@electron-forge/maker-zip",
       platforms: ["win32"],
@@ -34,7 +32,6 @@ module.exports = {
         name: "romsmanager-portable",
       },
     },
-    // Linux - AppImage (works on all distros including Arch)
     {
       name: "@reforged/maker-appimage",
       platforms: ["linux"],
@@ -51,48 +48,6 @@ module.exports = {
         },
       },
     },
-    /*
-    // Linux - Debian/Ubuntu (.deb)
-    {
-      name: "@electron-forge/maker-deb",
-      platforms: ["linux"],
-      config: {
-        options: {
-          bin: "romsmanager",
-          name: "romsmanager",
-          productName: "ROM Manager",
-          genericName: "ROM Manager",
-          description: "A desktop application for managing retro game ROMs",
-          icon: "./assets/icon.png",
-          maintainer: "andreakinder <andvillart@gmail.com>",
-          homepage: "https://github.com/andreakinder/RomsManager",
-          section: "utils",
-          categories: ["Utility", "Game"],
-          mimeType: ["x-scheme-handler/romsmanager"],
-        },
-      },
-    },
-    */
-    /*
-    // Linux - RPM (Fedora, RedHat, openSUSE, etc.)
-    {
-      name: "@electron-forge/maker-rpm",
-      platforms: ["linux"],
-      config: {
-        options: {
-          bin: "romsmanager",
-          name: "romsmanager",
-          productName: "ROM Manager",
-          genericName: "ROM Manager",
-          description: "A desktop application for managing retro game ROMs",
-          icon: "./assets/icon.png",
-          homepage: "https://github.com/andreakinder/RomsManager",
-          license: "MIT",
-          categories: ["Utility", "Game"],
-        },
-      },
-    },
-    */
   ],
   plugins: [
     {
@@ -118,8 +73,6 @@ module.exports = {
         },
       },
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
