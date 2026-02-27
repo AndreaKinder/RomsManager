6a87e112f33a8b8f319a55073d8a00421e6ae762
Andrea <andvillart@gmail.com>
2026-02-27 03:01:28 +0100

    refactor(webpack.main.config.js): convert require to ES6 imports

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,6 +1,10 @@
-const path = require("path");
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/window/main.js",
   entry: "./src/window/main.js",
   resolve: {
     alias: {
